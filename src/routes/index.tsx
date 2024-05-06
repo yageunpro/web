dfaf84c413386502ab1436a285571171296d2d05
dinohan <dinohan.dev@gmail.com>
2024-05-06 15:52:04 +0900

    feat: add default router paths

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,24 @@ const router = createBrowserRouter([
     path: "/",
     element: <div> Hi</div>,
   },
+  {
+    path: "/auth",
+    element: <div>Auth</div>,
+  },
+  {
+    path: "/appointments",
+    element: <div>Appointments</div>,
+    children: [
+      {
+        path: "/new",
+        element: <div>New</div>,
+      },
+      {
+        path: "/:id",
+        element: <div>Appointment</div>,
+      },
+    ],
+  },
 ]);
 
 export default router;
