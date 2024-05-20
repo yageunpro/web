91f87c7f89a1fdaf35d2d65e775e084c7fab7c8e
dinohan <dinohan.dev@gmail.com>
2024-05-20 15:21:32 +0900

    fix(Appointment): layout

--- src/features/appointment/Appointment.tsx
+++ src/features/appointment/Appointment.tsx
@@ -4,24 +4,27 @@ import styles from "./Appointment.module.scss";
 export function Appointment() {
   return (
     <>
-      <Title>내 약속</Title>
-      <label htmlFor="">이름</label>
-      <input type="text" />
+      <Title>한도협의 약속</Title>
 
-      <label htmlFor="">설명</label>
-      <textarea name="" id="" cols={30} rows={10}></textarea>
+      <section className={styles.information}>
+        <p className={styles.description}>
+          한도협 휴직 기념 홈파티 그런데 이제 술과 돼지 바베큐를 곁들인
+        </p>
 
-      <label htmlFor="">장소</label>
-      <input type="text" />
+        <label htmlFor="">장소</label>
+        <input type="text" />
 
-      <label htmlFor="">카테고리</label>
-      <input type="text" />
+        <label htmlFor="">카테고리</label>
+        <input type="text" />
 
-      <label htmlFor="">마감일</label>
-      <input type="date" />
+        <label htmlFor="">마감일</label>
+        <input type="date" />
 
-      <label htmlFor="">인원</label>
-      <div>태그</div>
+        <label htmlFor="">인원</label>
+        <div>태그</div>
+        <div>태그</div>
+        <div>태그</div>
+      </section>
     </>
   );
 }
