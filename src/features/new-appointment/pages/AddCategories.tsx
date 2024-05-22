b8242b24ca67592e9dcc0a88ee7b17475275da5b
dinohan <dinohan.dev@gmail.com>
2024-05-22 15:25:16 +0900

    feat: add map

--- src/features/new-appointment/pages/AddCategories.tsx
+++ src/features/new-appointment/pages/AddCategories.tsx
@@ -1,7 +1,15 @@
 import Title from "../../../components/Title";
 import { NextButton, NextButtonStyle } from "../components/NextButton";
+import {
+  Container as MapDiv,
+  NaverMap,
+  Marker,
+  useNavermaps,
+} from "react-naver-maps";
 
 export function AddCategories() {
+  const navermaps = useNavermaps();
+
   const handleClick = () => {
     alert("생성 API 내놔~!");
   };
@@ -9,6 +17,21 @@ export function AddCategories() {
   return (
     <>
       <Title>카테고리를 추가하세요</Title>
+      <MapDiv
+        style={{
+          width: "100%",
+          height: "400px",
+        }}
+      >
+        <NaverMap
+          defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
+          defaultZoom={15}
+        >
+          <Marker
+            defaultPosition={new navermaps.LatLng(37.3595704, 127.105399)}
+          />
+        </NaverMap>
+      </MapDiv>
       <NextButton>
         <button onClick={handleClick} className={NextButtonStyle}>
           완료
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.tsx";
 import "./index.css";
+import { NavermapsProvider } from "react-naver-maps";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <NavermapsProvider ncpClientId="fyrc4xrt6q">
+      <RouterProvider router={router} />
+    </NavermapsProvider>
   </React.StrictMode>
 );
