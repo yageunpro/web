e86067bd08c1d7f8664e9edcefdcfcfe4fb7efe2
dinohan <dinohan.dev@gmail.com>
2024-05-22 15:18:17 +0900

    fix(AddCategories): add complete button

--- src/features/new-appointment/pages/AddCategories.tsx
+++ src/features/new-appointment/pages/AddCategories.tsx
@@ -1,11 +1,19 @@
 import Title from "../../../components/Title";
-import { NextButton } from "../components/NextButton";
+import { NextButton, NextButtonStyle } from "../components/NextButton";
 
 export function AddCategories() {
+  const handleClick = () => {
+    alert("생성 API 내놔~!");
+  };
+
   return (
     <>
       <Title>카테고리를 추가하세요</Title>
-      <NextButton onClick={() => {}}>완료</NextButton>
+      <NextButton>
+        <button onClick={handleClick} className={NextButtonStyle}>
+          완료
+        </button>
+      </NextButton>
     </>
   );
 }
