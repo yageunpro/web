ab6059073addfeecada93aa109d507e8e642b484
dinohan <dinohan.dev@gmail.com>
2024-06-03 05:21:24 +0900

    feat

--- src/features/new-appointment/pages/ExcludedSchedule.tsx
+++ src/features/new-appointment/pages/ExcludedSchedule.tsx
@@ -1,9 +1,35 @@
-import { useRef } from "react";
+import { Fragment, useRef } from "react";
 import Title from "../../../components/Title";
 import { NextButton, NextButtonStyle } from "../components/NextButton";
 import { useSchedule } from "../hooks/useSchedule";
 import { Schedule } from "../../../types/Schedule";
 import { NewSchedule } from "../components/NewSchedule/NewSchedule";
+import { Separator } from "@/components/ui/separator";
+import { isSameDay, isSameMonth, isSameYear } from "date-fns";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+function formatDateTime(start: string, end: string) {
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+
+  const startFormat = Intl.DateTimeFormat("ko-KR", {
+    year: isSameYear(new Date(), startDate) ? undefined : "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+
+  const endFormat = Intl.DateTimeFormat("ko-KR", {
+    year: isSameYear(startDate, endDate) ? undefined : "numeric",
+    month: isSameMonth(startDate, endDate) ? undefined : "short",
+    day: isSameDay(startDate, endDate) ? undefined : "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+
+  return `${startFormat.format(startDate)} - ${endFormat.format(endDate)}`;
+}
 
 export function ExcludedSchedule() {
   const currentDate = useRef(new Date()).current;
@@ -26,12 +52,27 @@ export function ExcludedSchedule() {
         }}
       />
 
-      <ul>
-        {data &&
-          data.data.map((schedule: Schedule) => (
-            <li key={schedule.id}>{schedule.title}</li>
-          ))}
-      </ul>
+      <ScrollArea>
+        <ul className="flex flex-col mt-4 gap-2">
+          {data &&
+            data.data.map((schedule: Schedule, index: number) => {
+              const isFirst = index === 0;
+
+              return (
+                <Fragment key={schedule.id}>
+                  {!isFirst && <Separator className="ml-3" />}
+                  <li className="ml-3 flex flex-col">
+                    {schedule.title}
+
+                    <p>
+                      {formatDateTime(schedule.startTime, schedule.endTime)}
+                    </p>
+                  </li>
+                </Fragment>
+              );
+            })}
+        </ul>
+      </ScrollArea>
 
       <NextButton>
         <a className={NextButtonStyle} href="#3">
