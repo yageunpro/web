e7ba0ad0d9b68796185fc49ce2718fcc98fe2ab5
dinohan <dinohan.dev@gmail.com>
2024-06-02 15:10:06 +0900

    fix: AddCategories.tsx에 useState import 추가

--- src/features/new-appointment/pages/AddCategories/AddCategories.tsx
+++ src/features/new-appointment/pages/AddCategories/AddCategories.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import Title from "../../../../components/Title";
 import { NextButton, NextButtonStyle } from "../../components/NextButton";
 import { MapView } from "../../map";
@@ -11,13 +11,32 @@ export function AddCategories() {
     alert("생성 API 내놔~!");
   };
 
+  const [inputValue, setInputValue] = useState("");
+
+  const [categories, setCategories] = useState<string[]>([]);
+
+  const handleAddCategory = () => {
+    setCategories([...categories, inputValue]);
+    setInputValue("");
+  };
+
   return (
     <>
       <Title>카테고리를 추가하세요</Title>
 
       <div className={styles.wrapper}>
         <div className={styles.inputWrapper}>
-          <Input type="text" placeholder="카테고리를 입력해주세요." />
+          <Input
+            type="text"
+            placeholder="카테고리를 입력해주세요."
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleAddCategory();
+              }
+            }}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
         </div>
 
         <Suspense fallback={<div>Loading...</div>}>
