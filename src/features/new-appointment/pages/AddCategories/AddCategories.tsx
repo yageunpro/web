0da26847f67aa5d530fb07d6a0a1a8d1cfe4105c
dinohan <dinohan.dev@gmail.com>
2024-06-02 14:54:16 +0900

    fix: AddCategories style

---
+++ src/features/new-appointment/pages/AddCategories/AddCategories.tsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+import Title from "../../../../components/Title";
+import { NextButton, NextButtonStyle } from "../../components/NextButton";
+import { MapView } from "../../map";
+import { Input } from "../../../../components/Input";
+
+import styles from "./AddCategories.module.scss";
+
+export function AddCategories() {
+  const handleClick = () => {
+    alert("생성 API 내놔~!");
+  };
+
+  return (
+    <>
+      <Title>카테고리를 추가하세요</Title>
+
+      <div className={styles.wrapper}>
+        <div className={styles.inputWrapper}>
+          <Input type="text" />
+        </div>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <MapView />
+        </Suspense>
+
+        <NextButton>
+          <button onClick={handleClick} className={NextButtonStyle}>
+            완료
+          </button>
+        </NextButton>
+      </div>
+    </>
+  );
+}
