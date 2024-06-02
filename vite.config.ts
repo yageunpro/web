183e0b2fe1f5224db7bb9e3244335d9f50b7d299
dinohan <dinohan.dev@gmail.com>
2024-06-02 16:51:25 +0900

    chore: add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  alias: {
+    "@": path.resolve(__dirname, "./src"),
+  },
   server: {
     host: true,
     proxy: {
